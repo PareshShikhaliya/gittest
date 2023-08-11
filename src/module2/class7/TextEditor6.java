a33258c9873ecf3361d8606c13d5cbf29666766b
Paresh Shikhaliya <pareshshikhaliya2014@gmail.com>
2023-08-11 10:04:46 -0400

    mod 2 class 9

---
+++ src/module2/class7/TextEditor6.java
@@ -0,0 +1,39 @@
+package module2.class7;
+
+interface Plugin {
+    void execute();
+}
+
+class SpellCheckPlugin implements Plugin {
+    @Override
+    public void execute() {
+        System.out.println("Spell check is running...");
+    }
+}
+
+class GrammarCheckPlugin implements Plugin {
+    @Override
+    public void execute() {
+        System.out.println("Grammar check is running...");
+    }
+}
+
+public class TextEditor6 {
+    public static void main(String[] args) {
+        // Simulate loading plugins
+        Plugin spellCheckPlugin = new SpellCheckPlugin();
+        Plugin grammarCheckPlugin = new GrammarCheckPlugin();
+
+        // Assume these plugins are dynamically loaded based on user configuration
+
+        // Execute the loaded plugins
+        executePlugin(spellCheckPlugin);
+        executePlugin(grammarCheckPlugin);
+    }
+
+    static void executePlugin(Plugin plugin) {
+        System.out.println("Executing plugin: " + plugin.getClass().getSimpleName());
+        plugin.execute();
+        System.out.println();
+    }
+}
